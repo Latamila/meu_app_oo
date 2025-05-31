e4bf18f8dc9e182e32b2a5c035464f322183f209
Camila <camilaaeromoca@gmail.com>
2025-05-31 16:39:01 -0300

    orientado a objetos

---
+++ bin/meu_app_oo.dart
@@ -0,0 +1,11 @@
+import 'package:meu_app_oo/classes/Pessoa.dart';
+
+void main(List<String> argumentsj){
+
+  var p1 = new Pessoa();
+  p1.setNome('Danila');
+  p1.setEndereco('rua 1');
+
+  print(p1);
+
+}
