86ea61737eec4cde96695665b455b0d8c0583c7e
Camila <camilaaeromoca@gmail.com>
2025-05-31 20:08:05 -0300

    retirando o p1

--- bin/meu_app_oo.dart
+++ bin/meu_app_oo.dart
@@ -1,11 +1,7 @@
-import 'package:meu_app_oo/classes/pessoa.dart';
 import 'package:meu_app_oo/classes/pessoa_fisica.dart';
 import 'package:meu_app_oo/classes/pessoa_juridica.dart';
 
 void main(List<String> arguments){
-
-  var p1 = new Pessoa('Danila','rua 1');
-  print(p1);
 
   var p2 = new PessoaFisica('Camila','rua 1','22222');
   print(p2);
